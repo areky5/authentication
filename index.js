67af47ae595a9e4eb52d0b44e46605099eb1385a
Nicholas Chew <areky5@hotmail.com>
2017-04-18 20:09:21 +0800

    authentication server boiler plate

---
+++ index.js
@@ -0,0 +1,25 @@
+// Main starting point of the application
+// Express code
+
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB setup
+mongoose.connect('mongodb://localhost:/auth/auth');
+
+// App setup - express
+// Middleware in express - morgan,bodyParser
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// Server setup - express to the outside world
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
